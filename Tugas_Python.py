ea3205c7252e2d73d2c2f5e496ae275cac27f8b3
Hsyah12 <60025422+Hsyah12@users.noreply.github.com>
2022-11-07 09:51:33 +0700

    Add files via upload

---
+++ Tugas_Python.py
@@ -0,0 +1,33 @@
+#Soal 1 Hello World to Python
+x = "Hello World to Python"
+print(x)
+
+#Soal 2: Aritmatika di Python
+#- Buat statement pertambahan antara 2 number di Python
+#- Buat statement perkurangan antara 2 number di Python
+#- Buat statement perkalian antara 2 number di Python
+#- Buat statement pembagian antara 2 number di Python
+S2_1 = int (2 + 2)
+S2_2 = int (2 - 2)
+S2_3 = int (2 * 2)
+S2_4 = int (2 / 4)
+print(S2_1, S2_2, S2_3, S2_4)
+
+#Quiz 3: Assign Variable dan Tipe Data Integer, Float
+#- Buat suatu variabel a dan b, dimana a dan b adalah nilai bertipe data numeric
+#- Berikan suatu nilai bertipe data integer, hasil pembagian dari a dengan b
+#- Berikan suatu nilai bertipe data float, hasil pembagian dari a dengan b
+S3_1 = int (12)
+S3_2 = str ("EKAL GANTENG")
+S3_3 = float (10)
+Kumpulan = S3_1, S3_2, S3_3
+print(Kumpulan)
+
+
+#Soal 4: Tipe Data
+#Lengkapi code di bawah ini untuk menghasilkan output yang sesuai
+#usia = 
+#kata = 'usiaku: '
+usia = 17
+kata = "usiaku :"
+print(kata,usia)
